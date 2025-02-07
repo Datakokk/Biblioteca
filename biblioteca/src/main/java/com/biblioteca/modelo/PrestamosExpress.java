f5e96f51a6f31b5d03492259d2d9550d825aeaf6
Datakokk <fiodorelzarster@gmx.com>
2025-02-07 18:52:12 +0100

    Se ha implementado todos los modelos de nuestra base de datos

---
+++ biblioteca/src/main/java/com/biblioteca/modelo/PrestamosExpress.java
@@ -0,0 +1,28 @@
+package com.biblioteca.modelo;
+
+public class PrestamosExpress {
+	private int idExpress;
+	private int idPrestamo;
+	private int diasPrestamos;
+	
+	public int getIdExpress() {
+		return idExpress;
+	}
+	public void setIdExpress(int idExpress) {
+		this.idExpress = idExpress;
+	}
+	public int getIdPrestamo() {
+		return idPrestamo;
+	}
+	public void setIdPrestamo(int idPrestamo) {
+		this.idPrestamo = idPrestamo;
+	}
+	public int getDiasPrestamos() {
+		return diasPrestamos;
+	}
+	public void setDiasPrestamos(int diasPrestamos) {
+		this.diasPrestamos = diasPrestamos;
+	}
+	
+	
+}
---
+++ biblioteca/src/main/java/com/biblioteca/modelo/Reporte.java
@@ -0,0 +1,29 @@
+package com.biblioteca.modelo;
+
+import java.util.Date;
+
+public class Reporte {
+	private int idReporte;
+	private String descripcion;
+	private Date fechaGeneracion;
+	
+	
+	public int getIdReporte() {
+		return idReporte;
+	}
+	public void setIdReporte(int idReporte) {
+		this.idReporte = idReporte;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public Date getFechaGeneracion() {
+		return fechaGeneracion;
+	}
+	public void setFechaGeneracion(Date fechaGeneracion) {
+		this.fechaGeneracion = fechaGeneracion;
+	}
+}
