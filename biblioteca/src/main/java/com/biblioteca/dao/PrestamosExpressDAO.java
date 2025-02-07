d13210b0bdce1c1f5b3294ca4d9b9f029b55eeb0
Datakokk <fiodorelzarster@gmx.com>
2025-02-07 18:53:12 +0100

    Se ha finalizado todas las clases DAOs de nuestro proyecto

---
+++ biblioteca/src/main/java/com/biblioteca/dao/PrestamosExpressDAO.java
@@ -0,0 +1,50 @@
+package com.biblioteca.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.biblioteca.modelo.PrestamosExpress;
+
+public class PrestamosExpressDAO {
+
+	private Connection conexion;
+	
+	public PrestamosExpressDAO(Connection conexion) {
+		super();
+		this.conexion = conexion;
+	}
+	
+	// Agregar un nuevo prestamo express
+	public void agregarPrestamoExpress(PrestamosExpress express) throws SQLException {
+		String sql = "INSERT INTO prestamos_express (id_express, id_prestamo, dias_prestamos) VALUES (?, ?, ?)";
+		try(PreparedStatement ps = conexion.prepareStatement(sql)){
+			ps.setInt(1, express.getIdExpress());
+			ps.setInt(2, express.getIdPrestamo());
+			ps.setInt(3, express.getDiasPrestamos());
+			ps.executeUpdate();
+		}
+	}
+	
+	// Listamos todos los prestamos express
+	public List<PrestamosExpress> listarPrestamosExpress() throws SQLException {
+		List<PrestamosExpress> prestamosExpress = new ArrayList<>();
+		String sql = "SELECT * FROM prestamos_express";
+		try(Statement st = conexion.createStatement();
+			ResultSet rs = st.executeQuery(sql)){
+			while(rs.next()) {
+				PrestamosExpress express = new PrestamosExpress();
+				express.setIdExpress(rs.getInt("id_express"));
+				express.setIdPrestamo(rs.getInt("id_prestamos"));
+				express.setDiasPrestamos(rs.getInt("dias_prestamos"));
+				prestamosExpress.add(express);
+			}
+		}
+		return prestamosExpress;
+	}
+	
+}
